c1a74d0d0e58fbf76e5eac567fa57f3fdd49a741
Alex-RV <alexsandrr2005@gmail.com>
2022-11-05 14:12:30 -0700

    slider

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/test", methods=["POST"])
+def test():
+    slider_value = request.form["slider"]
+    return render_template ("index.html", slider_value=slider_value)
+
+if __name__ == '__main__':
+    app.run()
