fed4ba3a38c3b0ac759597d5f78aa7428975d5f1
Alex-RV <alexsandrr2005@gmail.com>
2022-11-05 12:54:11 -0700

    raspberry pi commit 2

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-app.run(host="",)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
